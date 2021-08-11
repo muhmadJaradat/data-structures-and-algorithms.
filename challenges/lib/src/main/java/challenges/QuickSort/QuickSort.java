2118d951c4c31d613b6a810eb57d3bbb99df0ee6
muhmadjaradat <muhmadjradat@gmail.com>
2021-08-11 23:43:50 +0300

    adding quick sort  challenge

---
+++ challenges/lib/src/main/java/challenges/QuickSort/QuickSort.java
@@ -0,0 +1,45 @@
+package challenges.QuickSort;
+
+public class QuickSort {
+
+    public void quickSort(int[] arr, int left, int right)
+    {
+        if (left < right)
+        {
+
+            int position = partition(arr, left, right);
+
+
+            quickSort(arr, left, position-1);
+            quickSort(arr, position+1, right);
+        }
+
+    }
+
+    int partition(int[] arr, int low, int high)
+    {
+        int pivot = arr[high];
+        int i = (low-1);
+        for (int j=low; j<high; j++)
+        {
+
+            if (arr[j] <= pivot)
+            {
+                i++;
+
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        int temp = arr[i+1];
+        arr[i+1] = arr[high];
+        arr[high] = temp;
+
+        return i+1;
+    }
+
+
+
+}
